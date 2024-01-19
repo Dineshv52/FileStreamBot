9e1bc27d2e8b0c36f234e04f59586962bcdf6ab0
Dinesh V <dinesh.v@zohocorp.com>
2024-01-19 22:39:14 +0530

    shorturl function added

--- FileStream/utils/bot_utils.py
+++ FileStream/utils/bot_utils.py
@@ -93,7 +93,7 @@ def shorturl(file_link, file_name):
             if shortened_url:
                 shortlink = "🚀 Fᴀsᴛ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ 🔗 " + shortened_url
                 full_file_name = file_name + "\n" + shortlink
-                full_file_name = full_file_name + "\n ➥ ❤️❤❤Jᴏɪɴ : @movies_all_HUb ❤❤❤"
+                full_file_name = full_file_name +  "\n ➥ ❤️❤❤Jᴏɪɴ : @movies_all_HUb ❤❤❤"
                 return full_file_name
             else:
                 return file_link
@@ -117,14 +117,14 @@ async def gen_link(_id):
 
     if "video" in mime_type:
         stream_text = LANG.STREAM_TEXT.format(file_name, file_size, stream_link, page_link, file_link)
-        # reply_markup = InlineKeyboardMarkup(
-        #     [
-        #         [InlineKeyboardButton("sᴛʀᴇᴀᴍ", url=page_link), InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ", url=stream_link)],
-        #         [InlineKeyboardButton("ɢᴇᴛ ғɪʟᴇ", url=file_link),
-        #          InlineKeyboardButton("ʀᴇᴠᴏᴋᴇ ғɪʟᴇ", callback_data=f"msgdelpvt_{_id}")],
-        #         [InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]
-        #     ]
-        # )
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("sᴛʀᴇᴀᴍ", url=page_link), InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ", url=stream_link)],
+                [InlineKeyboardButton("ɢᴇᴛ ғɪʟᴇ", url=file_link),
+                 InlineKeyboardButton("ʀᴇᴠᴏᴋᴇ ғɪʟᴇ", callback_data=f"msgdelpvt_{_id}")],
+                [InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]
+            ]
+        )
     else:
         stream_text = LANG.STREAM_TEXT_X.format(file_name, file_size, stream_link, file_link)
         reply_markup = InlineKeyboardMarkup(
