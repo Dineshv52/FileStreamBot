2f951b4a2d28cdee0bce2f5784edbbbd0d5d8188
Dinesh V <dinesh.v@zohocorp.com>
2024-01-19 23:37:26 +0530

    shorturl function added

--- FileStream/utils/bot_utils.py
+++ FileStream/utils/bot_utils.py
@@ -91,7 +91,7 @@ def shorturl(file_link):
             response_data = response.json()
             shortened_url = response_data.get('shortenedUrl', None)
             if shortened_url:
-                shortlink = "Fᴀsᴛ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ  " + shortened_url
+                shortlink = "Fᴀsᴛ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ " + shortened_url
                 # full_file_name = file_name + shortlink
                 # full_file_name = full_file_name +  "\n ➥ ❤️❤❤Jᴏɪɴ : @movies_all_HUb ❤❤❤"
 
@@ -114,7 +114,7 @@ async def gen_link(_id):
     stream_link = f"{Server.URL}dl/{_id}"
     file_link = f"https://t.me/{FileStream.username}?start=file_{_id}"
 
-    # file_link = shorturl(file_link)
+    file_link = shorturl(file_link)
 
     if "video" in mime_type:
         stream_text = LANG.STREAM_TEXT.format(file_name, file_size, stream_link, page_link, file_link)
